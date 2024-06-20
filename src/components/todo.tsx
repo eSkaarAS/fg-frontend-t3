9734aeeac8b4100f0f77c01955abbae67f1377bb
Erik Skaar <erik.skaar@kantega.no>
2024-06-20 08:12:39 +0200

    implement todo component

---
+++ src/components/todo.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Checkbox } from "@/components/ui/checkbox";
+import { api } from "@/utils/api";
+import { type Todo } from "@prisma/client";
+
+export default function Todo({ todo }: { todo: Todo }) {
+  const utils = api.useUtils();
+  const { mutate: toggleTodo } = api.todo.toggle.useMutation({
+    onSuccess: () => {
+      void utils.todo.findAll.invalidate();
+    },
+  });
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Checkbox
+        id={todo.id}
+        defaultChecked={todo.isCompleted}
+        onCheckedChange={(e) => {
+          if (typeof e === "boolean")
+            toggleTodo({
+              id: todo.id,
+              completed: e,
+            });
+        }}
+      />
+      <label
+        htmlFor={todo.id}
+        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+      >
+        {todo.description}
+      </label>
+    </div>
+  );
+}
