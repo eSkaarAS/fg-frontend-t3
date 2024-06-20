437db023ecdd3f87a086f86780a6ec03202328a0
Erik Skaar <erik.skaar@kantega.no>
2024-06-20 08:15:57 +0200

    implement create-todo component

---
+++ src/components/create-todo.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { api } from "@/utils/api";
+
+const FormSchema = z.object({
+  description: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+export default function CreateTodo() {
+  const utils = api.useUtils();
+
+  const { mutate: createTodo } = api.todo.create.useMutation({
+    onSuccess: () => {
+      void utils.todo.findAll.invalidate();
+    },
+  });
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      description: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    createTodo(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input placeholder="todo..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
